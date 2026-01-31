f1d989e66ad84f4d306e50570f130c4dab424b60
Azahara-dev <azaharatalaverat@gmail.com>
2026-01-31 12:11:36 +0100

    Lemonade - Implement click logic to progress through stages

--- Lemonade/app/src/main/java/com/example/lemonade/MainActivity.kt
+++ Lemonade/app/src/main/java/com/example/lemonade/MainActivity.kt
@@ -6,19 +6,27 @@ import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.painter.Painter
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
@@ -42,27 +50,60 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun ImageWithButtonAndText(modifier: Modifier = Modifier) {
+    var lemonadeState by remember { mutableStateOf(1) }
+    var count by remember { mutableStateOf(0) }
+    val imagenResource = when (lemonadeState) {
+        1 -> R.drawable.lemon_tree
+        2 -> R.drawable.lemon_squeeze
+        3 -> R.drawable.lemon_drink
+        else -> R.drawable.lemon_restart
+    }
+    val imagenDescription = when (lemonadeState) {
+        1 -> R.string.lemon_tree_content_description
+        2 -> R.string.lemon_content_description
+        3 -> R.string.glass_lemonade_content_description
+        else -> R.string.empty_glass_content_description
+    }
+
+    val textResource = when (lemonadeState) {
+        1 -> R.string.lemon_tree_text
+        2 -> R.string.lemon_text
+        3 -> R.string.glass_lemonade_text
+        else -> R.string.empty_glass_text
+    }
+
     Column(
         modifier = modifier,
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
 
         Image(
-            painterResource(R.drawable.lemon_tree),
-            contentDescription = stringResource(R.string.lemon_tree_content_description),
+            painterResource(imagenResource),
+            contentDescription = stringResource(imagenDescription),
             modifier = Modifier
                 .clip(RoundedCornerShape(20.dp))
                 .background(color = Color(0xFFC2D9B0))
+                .clickable {
+                    if (lemonadeState == 1) {
+                        lemonadeState++
+                        count = (2..4).random()
+                    } else if (lemonadeState == 2){
+                        count--
+                        if (count == 0) {
+                            lemonadeState++
+                        }
+                    } else if (lemonadeState == 4) {
+                        lemonadeState = 1
+                    } else {
+                        lemonadeState++
+                    }
+                }
         )
-
-        /*Button(onClick = { /*TODO*/ }) {
-
-        }*/
 
         Spacer(modifier = Modifier.height(16.dp))
 
         Text(
-            text = stringResource(R.string.lemon_tree_text),
+            text = stringResource(textResource),
             fontSize = 18.sp
         )
     }
