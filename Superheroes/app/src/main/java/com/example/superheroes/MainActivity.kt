9a411aa50c34192095a2dc4c3b264f776fd4d42e
Azahara-dev <azaharatalaverat@gmail.com>
2026-02-09 13:58:02 +0100

    Superheroes - Implement Heroes screen and display hero list

--- Superheroes/app/src/main/java/com/example/superheroes/MainActivity.kt
+++ Superheroes/app/src/main/java/com/example/superheroes/MainActivity.kt
@@ -11,6 +11,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.example.superheroes.model.HeroesDataSource
 import com.example.superheroes.ui.theme.SuperheroesTheme
 
 class MainActivity : ComponentActivity() {
@@ -20,28 +21,9 @@ class MainActivity : ComponentActivity() {
         setContent {
             SuperheroesTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                    HeroesList(HeroesDataSource.HeroesRepository.heroes)
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    SuperheroesTheme {
-        Greeting("Android")
     }
 }
